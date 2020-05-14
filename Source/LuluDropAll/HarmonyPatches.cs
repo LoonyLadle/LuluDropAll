e096d4874c77b67591170d614168d155a932b5b2
LoonyLadle <LoonyLadle@users.noreply.github.com>
2020-05-14 02:51:22 -0400

    recompile for 1.1

--- Source/LuluDropAll/HarmonyPatches.cs
+++ Source/LuluDropAll/HarmonyPatches.cs
@@ -1,4 +1,4 @@
-﻿using Harmony;
+﻿using HarmonyLib;
 using System.Collections.Generic;
 using UnityEngine;
 using Verse;
@@ -7,41 +7,41 @@ using Verse;
 
 namespace LoonyLadle.DropAll
 {
-    [StaticConstructorOnStartup]
-    static class HarmonyPatches
-    {
-        static HarmonyPatches()
-        {
-            var harmony = HarmonyInstance.Create("rimworld.loonyladle.dropall");
-            harmony.PatchAll();
-        }
-    }
+	[StaticConstructorOnStartup]
+	static class HarmonyPatches
+	{
+		static HarmonyPatches()
+		{
+			Harmony harmony = new Harmony("rimworld.loonyladle.dropall");
+			harmony.PatchAll();
+		}
+	}
 
-    [HarmonyPatch(typeof(Pawn), nameof(Pawn.GetGizmos))]
-    public static class HarmonyPatch_Pawn_GetGizmos
-    {
-        public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> gizmos, Pawn __instance)
-        {
-            foreach (Gizmo gizmo in gizmos)
-            {
-                yield return gizmo;
-            }
-            
-            if (__instance.Spawned && __instance.MentalState == null && __instance.HostFaction == null && (__instance.Faction?.IsPlayer ?? false) && (__instance.inventory?.innerContainer?.Any ?? false))
-            {
-                yield return new Command_Action()
-                {
-                    defaultLabel = "LuluDropAll_CommandDropAllLabel".Translate(),
-                    defaultDesc = "LuluDropAll_CommandDropAllDesc".Translate(),
-                    icon = ContentFinder<Texture2D>.Get("UI/Buttons/Drop"),
-                    action = delegate()
-                    {
-                        __instance.inventory.DropAllNearPawn(__instance.PositionHeld);
-                    }
-                };
-            }
+	[HarmonyPatch(typeof(Pawn), nameof(Pawn.GetGizmos))]
+	public static class HarmonyPatch_Pawn_GetGizmos
+	{
+		public static IEnumerable<Gizmo> Postfix(IEnumerable<Gizmo> gizmos, Pawn __instance)
+		{
+			foreach (Gizmo gizmo in gizmos)
+			{
+				yield return gizmo;
+			}
 
-            yield break;
-        }
-    }
+			if (__instance.Spawned && __instance.MentalStateDef == null && __instance.HostFaction == null && (__instance.Faction?.IsPlayer ?? false) && (__instance.inventory?.innerContainer?.Any ?? false))
+			{
+				yield return new Command_Action()
+				{
+					defaultLabel = "LuluDropAll_CommandDropAllLabel".Translate(),
+					defaultDesc = "LuluDropAll_CommandDropAllDesc".Translate(),
+					icon = ContentFinder<Texture2D>.Get("UI/Buttons/Drop"),
+					action = delegate()
+					{
+						__instance.inventory.DropAllNearPawn(__instance.PositionHeld);
+					}
+				};
+			}
+
+			yield break;
+		}
+	}
 }
